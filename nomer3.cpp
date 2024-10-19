64d12b1c73315224353c44de84788e60884ad472
Vladislava <122866844+SleEpyHeDgEhOg6@users.noreply.github.com>
2024-10-19 14:16:57 +0300

    Create nomer3.cpp

---
+++ nomer3.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+int ma(std::vector<int>& massive){
+    int maxm = 0;
+    for (int rt:massive){
+        if (rt>maxm){
+            maxm=rt;
+        }
+    }
+    return maxm;
+};
+
+
+
+
+
+int argmax(std::vector<int>& massive){
+    int m=ma(massive);
+    int ind=0;
+    for (int i=0;i<massive.size();++i){
+        if(massive[i]==m){
+            ind=i;
+        }
+    }
+    return ind;
+};
+
+int main(){
+    int mas=5;
+
+    std::vector<int> l(mas);
+    std::random_device rd;  // позволяет получить случайное целое число, которое мы использовали для выбора последовательности основного генератора mt19937
+    std::mt19937 gen(rd());  
+    std::uniform_int_distribution<> dist(1, 100);  // диапазон случайных чисел от 1 до 100,uniform_int_distribution-озволяет генерировать равномерное целочисленное распределение
+
+    // заполняем массив случайными числами
+    for (int i = 0; i < mas; ++i) {
+        l[i] = dist(gen);  
+        std::cout << l[i] << " ";
+    }
+    std::cout<<" "<<std::endl;
+    int otvet=argmax(l);
+    std::cout<<otvet<<std::endl;
+    return 0;
+}
